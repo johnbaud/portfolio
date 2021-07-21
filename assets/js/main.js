7108fea6838a3e3d35da59316bdbac694e52728a
baud jonathan <johnbaud@hotmail.fr>
2021-07-21 11:11:04 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+//apparition header
+setTimeout(function(){
+  document.querySelector('#top').style.opacity="1";
+},500);
+
+// TextScramble code
+class TextScramble {
+    constructor(el) {
+      this.el = el;
+      this.chars = "123456789!<>-_\\/[]{}—=+*^?#________";
+      this.update = this.update.bind(this);
+    }
+    setText(newText) {
+      const oldText = this.el.innerText;
+      const length = Math.max(oldText.length, newText.length);
+      const promise = new Promise((resolve) => this.resolve = resolve);
+      this.queue = [];
+      for (let i = 0; i < length; i++) {
+        const from = oldText[i] || '';
+        const to = newText[i] || '';
+        const start = Math.floor(Math.random() * 40);
+        const end = start + Math.floor(Math.random() * 40);
+        this.queue.push({ from, to, start, end });
+      };
+      cancelAnimationFrame(this.frameRequest);
+      this.frame = 0;
+      this.update();
+      return promise;
+    };
+    update() {
+      let output = '';
+      let complete = 0;
+      for (let i = 0, n = this.queue.length; i < n; i++) {
+        let { from, to, start, end, char } = this.queue[i];
+        if (this.frame >= end) {
+          complete++;
+          output += to;
+        } else if (this.frame >= start) {
+          if (!char || Math.random() < 0.28) {
+            char = this.randomChar();
+            this.queue[i].char = char;
+          }
+          output += `<span class="dud">${char}</span>`;
+        } else {
+          output += from;
+        }
+      };
+      this.el.innerHTML = output;
+      if (complete === this.queue.length) {
+        this.resolve();
+      } else {
+        this.frameRequest = requestAnimationFrame(this.update);
+        this.frame++;
+      }
+    }
+    randomChar() {
+      return this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+  };
+  // text scramble
+  // application scramble
+  const phrases = ["Développeur web - graphiste"]
+  
+  const el = document.querySelector('.text');
+  const fx = new TextScramble(el);
+  
+  let counter = 0;
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next,7000)
+      
+      
+    });
+    counter = (counter + 1) % phrases.length 
+    
+  };
+  setTimeout(function(){
+     next();
+  },3000);
+ 
+
+//fleche de retour
+window.addEventListener("scroll", function () {
+    //distance entre le haut et le scrool effectué
+    //console.log("window.scrollY");
+
+
+    if (window.scrollY > 600) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+
+});
+//fleche de retour
+
+//thumbnail overlay
+let mesThumbnails = document.querySelectorAll("div.overlay");
+  console.log(mesThumbnails);
+
+for (let element of mesThumbnails) {
+    element.addEventListener("mouseenter", function(){
+    console.log('repéré');
+    element.style.opacity = "1";
+    element.style.transition = "0.2s ease"
+    })
+    element.addEventListener("mouseout", function(){
+    console.log('repéré');
+    element.style.opacity = "0";
+    })
+    
+};
+
+//thumbnail overlay
